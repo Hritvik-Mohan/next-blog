ce1c73f79451611edbd57c696f8bdaef2681b3be
Hritvik Mohan <hritvik5005@gmail.com>
2023-09-30 18:55:43 +0530

    Update

--- app/signup/page.js
+++ app/signup/page.js
@@ -3,14 +3,16 @@ import Link from "next/link";
 export default function Login() {
   return (
     <form className="login">
-      <h2 className="">Login</h2>
+      <h2 className="">Sign Up</h2>
       <label>Username</label>
+      <input type="text" />
+      <label>Email</label>
       <input type="text" />
       <label>Password</label>
       <input type="password" />
       <input type="submit" className="submit" />
       <div>
-          <Link href="signup">Create a new account.</Link>
+          <Link href="login">Already have an account?</Link>
       </div>
     </form>
   );
